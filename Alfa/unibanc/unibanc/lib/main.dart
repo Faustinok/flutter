a7ccbee90b0784bce4b9e20ba1a892d11d30bb75
gabriel <faustinoknada@gmail.com>
2020-04-02 17:03:27 -0300

    Criação das telas de cadastro e inclusao do fluxo.

--- Alfa/unibanc/unibanc/lib/main.dart
+++ Alfa/unibanc/unibanc/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:unibanc/ui/home.dart';
 import 'package:unibanc/ui/login.dart';
+import 'package:unibanc/ui/transferencia1.dart';
 import 'package:unibanc/user/user.dart';
 void main() {
   User user = new User("Gabriel", "Carvalho", 236306, "faustinoknada@gmail.com", "123", 1000.00);
--- Alfa/unibanc/unibanc/lib/ui/home.dart
+++ Alfa/unibanc/unibanc/lib/ui/home.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:unibanc/ui/transferencia1.dart';
 import 'package:unibanc/user/user.dart';
 
 import 'login.dart';
 
 class Home extends StatelessWidget {
-
   User user;
   Home(this.user);
 
   @override
   Widget build(BuildContext context) {
-      double _height =MediaQuery.of(context).size.height;
-  double _width =MediaQuery.of(context).size.width;
+    double _height = MediaQuery.of(context).size.height;
+    double _width = MediaQuery.of(context).size.width;
     return Scaffold(
         backgroundColor: Colors.grey,
         appBar: AppBar(
@@ -30,71 +30,85 @@ class Home extends StatelessWidget {
             )
           ],
         ),
-        body: 
-        Column(
+        body: Column(
           children: <Widget>[
             Row(
               children: <Widget>[
                 Container(
-                  height: 70,
-                  width: 180,
-                  decoration: BoxDecoration(
-                    
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 0.5,
-                    ), 
-                  ),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Icon(Icons.attach_money, color: Colors.white,size: 35,),
-                      Text(user.debito.toString(), style: TextStyle(fontSize: 32),)
-                    ],
-                  )
-                ),
-                 Container(
-                  height: 70,
-                  width: 180,
-                  decoration: BoxDecoration(
-                    
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 0.5,
-                    ), 
-                  ),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Text("Conta ", style: TextStyle(fontSize: 20),),
-                      Text(user.conta.toString(), style: TextStyle(fontSize: 32),)
-                    ],
-                  )
-                ),
+                    height: 70,
+                    width: 180,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 0.5,
+                      ),
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.attach_money,
+                          color: Colors.white,
+                          size: 35,
+                        ),
+                        Text(
+                          user.debito.toString(),
+                          style: TextStyle(fontSize: 32),
+                        )
+                      ],
+                    )),
+                Container(
+                    height: 70,
+                    width: 180,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 0.5,
+                      ),
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "Conta ",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        Text(
+                          user.conta.toString(),
+                          style: TextStyle(fontSize: 32),
+                        )
+                      ],
+                    )),
               ],
             ),
             Padding(
-              padding: EdgeInsets.only(top:50),
-              child: Card(          
-                  child:ListTile(
-                    title: Text("Transferencia"),
-                  ),
-                  ),
-                
+              padding: EdgeInsets.only(top: 50),
+              child: Card(
+                child: ListTile(
+                  title: Text("Transferencia"),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        (MaterialPageRoute(
+                            builder: (context) => Transferencia1()
+                            )
+                        )
+                      );
+                  },
+                ),
               ),
-               Card(          
-                  child:ListTile(
-                    title: Text("Extrato"),
-                  ),
-                  ),
-               Card(          
-                  child:ListTile(
-                    title: Text("Perfil"),
-                  ),
-                  ),                                   
+            ),
+            Card(
+              child: ListTile(
+                title: Text("Extrato"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text("Perfil"),
+              ),
+            ),
           ],
-        )
-        
-        );
+        ));
   }
 }

775617d42ea8406fd7c15278cd9f4985ec7550db
gabriel <faustinoknada@gmail.com>
2020-04-06 19:00:38 -0300

    Conclusão do processo de extrato.dart.

--- Alfa/unibanc/unibanc/lib/ui/extrato.dart
+++ Alfa/unibanc/unibanc/lib/ui/extrato.dart
@@ -5,19 +5,22 @@ import 'package:unibanc/user/user.dart';
 class Extrato extends StatelessWidget {
   User user;
   Extrato(this.user);
-  UserMov mov = new UserMov.vazio();
-  int conta;
-  List<UserMov> movimentacoes = new List();
- getMov(int id) async {
- movimentacoes =await mov.selectMov(user.id);
- print(movimentacoes);
- 
- }
+  UserMov mov = new UserMov.vazio(); 
+  List<UserMov> movs = List();
   
+  getmovimento() async {
+     movs= await mov.selectMov2(user.id);
+     movs.length;
+  }
   
-
   @override
   Widget build(BuildContext context) {
+    
+    getmovimento();
+    
+    
+    
+    
     return Scaffold(
       backgroundColor: Colors.grey,
       appBar: AppBar(
@@ -26,20 +29,84 @@ class Extrato extends StatelessWidget {
         centerTitle: true,
 
       ),
-      body:            Padding(
-              padding: const EdgeInsets.only(top: 20.0),
-              child: RaisedButton(
-                color: Color.fromRGBO(159, 33, 29, 1.0),
-                padding: EdgeInsets.all(15.0),
-                child: Text("Transferir", style: TextStyle(fontSize: 20.0)),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                ),
-                onPressed: () {
-                  getMov(user.id);
+      body: FutureBuilder(
+        initialData: List(),
+        future: Future.delayed(Duration(seconds: 2)).then((value)=> mov.selectMov2(user.id)),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            
+            case ConnectionState.none:
+              // TODO: Handle this case.
+              break;
+            case ConnectionState.waiting:
+               return Center(
+                 child: Column(
+                   crossAxisAlignment: CrossAxisAlignment.center,
+                   mainAxisAlignment: MainAxisAlignment.center,
+                   children: <Widget>[
+                     CircularProgressIndicator(),
+                     Text("Carregando")
+                   ],
+                 ),
+               );              
+              break;
+            case ConnectionState.active:
+              // TODO: Handle this case.
+              break;
+            case ConnectionState.done:
+              List<UserMov> movimento = snapshot.data ;
+              //print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH ${snapshot.data}");
+
+              return ListView.builder(
+                itemCount: movimento.length,
+                itemBuilder: (context,indice){
+                  UserMov usermov = movimento[indice];
+                  
+                  return Card(                        
+            child: criaCard(usermov)
+          );
                 },
-              ),
-            ),
+              );
+              break;
+          }
+        })
     );
+  }
+  criaCard(UserMov userm) {
+    if (userm.tipo ==0) {
+      return Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(userm.nomeFrom),
+                Icon(Icons.arrow_forward, color: Colors.red,),
+                Text(userm.nomeTo),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Text(userm.valor.toString()),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Text(userm.date),
+                ),
+              ],
+            );
+    } else {
+      return Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(userm.nomeTo),
+                Icon(Icons.arrow_back, color: Colors.green,),
+                Text(userm.nomeFrom),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Text(userm.valor.toString()),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Text(userm.date),
+                ),
+              ],
+            );
+            }
   }
 }
--- Alfa/unibanc/unibanc/lib/user/UserMov.dart
+++ Alfa/unibanc/unibanc/lib/user/UserMov.dart
@@ -55,21 +55,18 @@ static Database _db;
   }
 
   Future<List<UserMov>>selectMov(int id) async {
-    var client = await db;
-    UserMov usermov;
-    final Future<List<Map<String, dynamic>>> futureMaps = 
-client.rawQuery("select user_From.nome as nomeFrom, user_to.nome as nomeTo, mov.valor as valor, date(dt_mov) as dataMov, 0 as tipo from usuario user_From inner join movimentacao mov on user_From.id =mov.id_from inner join usuario user_to on user_to.id =mov.id_to where user_From.id = $id;"
-);
- 
-    var maps = await futureMaps;
-    if (maps.length != 0) {
-      List.generate(maps.length, (i){
-    return UserMov.fromMap(maps[i]);
-      });     
-    } else {
-      return null;
-    } 
-     
+    return initDb().then((db){
+      db.rawQuery("select user_From.nome as nomeFrom, user_to.nome as nomeTo, mov.valor as valor, date(dt_mov) as dataMov, 0 as tipo from usuario user_From inner join movimentacao mov on user_From.id =mov.id_from inner join usuario user_to on user_to.id =mov.id_to where user_From.id = $id "
+).then((maps){
+  List<UserMov> movimentacoes = List();
+    for (Map<String, dynamic> map in maps) { 
+      movimentacoes.add(UserMov.fromMap(map));
+    }
+    print(movimentacoes[0]._date);
+    return movimentacoes;
+});
+    });
+         
   }
      Future<int> selectCount() async {
     var client = await db; 
@@ -83,5 +80,20 @@ client.rawQuery("select user_From.nome as nomeFrom, user_to.nome as nomeTo, mov.
     }
      
   }
-  
+       Future<List<UserMov>> selectMov2(int id) async {
+    var client = await db; 
+    int bla;
+    UserMov usermov;
+    final Future<List<Map<String, dynamic>>> futureMaps =  client.rawQuery("select user_From.nome as nomeFrom, user_to.nome as nomeTo, mov.valor as valor, date(dt_mov) as dataMov, 0 as tipo from usuario user_From inner join movimentacao mov on user_From.id =mov.id_from inner join usuario user_to on user_to.id =mov.id_to where user_From.id = $id union all select user_From.nome as nomeFrom, user_to.nome as nomeTo, mov.valor as valor, date(dt_mov) as dataMov, 1 as tipo from usuario user_From inner join movimentacao mov on user_From.id =mov.id_from inner join usuario user_to on user_to.id =mov.id_to where user_to.id = $id order by dataMov;") ;
+    var maps = await futureMaps;
+    List<UserMov> movimentacoes = List();
+    if(maps.length !=0 ) {
+      for (Map<String, dynamic> map in maps) {   
+      usermov = UserMov.fromMap(map);
+      movimentacoes.add(usermov);
+      }
+      return movimentacoes;
+    }
+     
+  }
 }
